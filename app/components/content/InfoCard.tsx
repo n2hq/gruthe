39559b8d94629ce6b79924363c35be2dd796f1a5
n2hq <n2hq.co@gmail.com>
2026-03-09 23:01:13 +0100

    Production: update commit

--- app/components/content/InfoCard.tsx
+++ app/components/content/InfoCard.tsx
@@ -74,6 +74,7 @@ const InfoCard = ({ item, isFirst = false }: { item: ListingType, isFirst?: bool
             let imgPath: string = config?.IMG_BASE_URL + img
             const prefetch = prefetchImage(imgPath)
             if (!prefetch) {
+                console.log('here')
                 setImgPath(`https://dynamic-media-cdn.tripadvisor.com/media/photo-o/30/eb/f5/fd/caption.jpg?w=1200&h=1200&s=1`)
             }
             else {

5f6c3a3d3c455f336902aaa2cbe0ab7c55b16d8a
n2hq <n2hq.co@gmail.com>
2026-03-10 15:23:58 +0200

    Production: update commit

--- app/root.tsx
+++ app/root.tsx
@@ -111,7 +111,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/bycetlogo.png?v=3" type="image/png" />
+        <link rel="icon" href="/gruthe5.png?v=3" type="image/png" />
         {
           import.meta.env.VITE_ENV === "prod" && (
             <script
